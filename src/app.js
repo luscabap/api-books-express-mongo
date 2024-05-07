ee99bbc0d92c443374c86a26d67275ab5fb3e439
luscabap <lucasbaptistasilva.dev@gmail.com>
2024-05-07 16:49:51 -0300

    Searching specify book and editing it

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express'
 
 const app = express();
+app.use(express.json())
 
 const livros = [
     {
@@ -13,6 +14,16 @@ const livros = [
     }
 ]
 
+function buscaLivro(id) {
+    return livros.findIndex(livro => {
+        return livro.id === Number(id)
+    })
+}
+
+function filtraLivro(id) {
+    return livros.filter(livro => livro.id === Number(id))
+}
+
 app.get('/', (req, res) => {
     res.status(200).send("Curso de Node.js");
 });
@@ -20,5 +31,34 @@ app.get('/', (req, res) => {
 app.get('/livros', (req, res) => {
     res.status(200).json(livros)
 });
+
+app.get('/livros/:id', (req, res) => {
+    // const index = buscaLivro(req.params.id)
+    // res.status(200).send(livros[index])
+
+    const livroFiltrado = filtraLivro(req.params.id)
+    if (livroFiltrado.length === 0) {
+        return res.status(404).send("Livro não encontrado :(")
+    }
+    res.status(200).send(livroFiltrado)
+})
+
+app.post("/livros", (req, res) => {
+    livros.push(req.body);
+    res.status(201).send("Livro adicionado com sucesso!");
+})
+
+app.put("/livros/:idLivroASerAlterado", (req, res) => {
+    const livroFiltrado = filtraLivro(req.params.idLivroASerAlterado);
+    if (livroFiltrado.length === 0) {
+        res.status(404).send("Livro não encontrado. Por favor, verifique novamente.")
+    }
+    livroFiltrado.map(item => item.titulo = req.body.titulo );
+    res.status(200).json(livros);
+    
+    // const index = buscaLivro(req.params.idLivroASerAlterado)
+    // livros[index].titulo = req.body.titulo;
+    // res.status(200).json(livros)
+})
 
 export default app;
