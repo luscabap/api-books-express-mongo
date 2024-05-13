0bf1b7e2f22d231227e25bd1d5b3b582f4766b88
luscabap <lucasbaptistasilva.dev@gmail.com>
2024-05-13 17:13:37 -0300

    PORT var .env

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 import "dotenv/config";
 import app from "./src/routes/app.js";
 
-const PORT = 3000;
+const PORT = process.env.DB_PORT || 3000;
+
 
 app.listen(PORT, () => {
   console.log(`Servidor escutando em http://localhost:${PORT}`);
